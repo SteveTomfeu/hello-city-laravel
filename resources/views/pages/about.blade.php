006707aa4f5d7dae415bc7c19b17d52db6bc8d52
Tomfeu Steve <stevefotsing@yahoo.fr>
2021-09-29 11:34:15 +0100

    Remove dupliation by using base layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About Us | Hello City</title>
-        
-    </head>
-    <body class="antialiased">
+@section('content')
 
        <p>Built with &hearts; by Steve Tomfeu.</p>
        <p><a href="/">Revenir à la page d'accueil</a></p>
-       <footer>
-           <p>&copy; Copyright {{ date('Y')}} </p>
-       </footer>
-    </body>
-</html>
+       
+@endsection
