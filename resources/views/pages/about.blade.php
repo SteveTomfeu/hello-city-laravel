7c3b0900ad7d80b5cb6756bec2e2662ac44235f3
Tomfeu Steve <stevefotsing@yahoo.fr>
2022-04-01 20:28:50 +0100

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'About Us | '.env('APP_NAME'))
 
 @section('content')
-
+<img src="/img/stevie.jpg" alt="Stevie Pictures">
        <p>Built with &hearts; by Steve Tomfeu Fotsing.</p>
        <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
        
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,7 @@
 
 
 @section('content')
+       <img src="/img/cmr.jpg" alt="Cameroun Flag">
        <h1>Hello from Cameroon !</h1>
        <p>It's currently {{ date('h:i A')}}.</p>
 @endsection
