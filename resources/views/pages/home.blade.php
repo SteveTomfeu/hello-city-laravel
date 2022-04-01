73cfe0814a1bd5f5ae3a298dbaeebed5a266fae5
Tomfeu Steve <stevefotsing@yahoo.fr>
2022-04-01 20:47:02 +0100

    Make use of asset function helper

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-       <img src="/img/cmr.jpg" alt="Cameroun Flag">
+       <img src=" {{ asset('img/cmr.jpg') }}" alt="Cameroun Flag">
        <h1>Hello from Cameroon !</h1>
        <p>It's currently {{ date('h:i A')}}.</p>
 @endsection
